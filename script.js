d0cb82147afd2d1f80d2340e7510a7634bfd8e94
Alex Curran <alexcurran.va@gmail.com>
2020-11-21 10:54:47 -0600

    Fixed rings

--- script.js
+++ script.js
@@ -1,85 +1,81 @@
 "use strict";
 
 /** @type {HTMLCanvasElement} */
-const canvas = document.querySelector(".canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector(`canvas`);
+const ctx = canvas.getContext(`2d`);
 
-// Rings Setup
-const numOfRings = 1;
-const maxWavesAmplitude = 17;
-const numOfWavesPerRing = 7;
-const ringRadius = 200;
-let ringsArr;
-let centerX;
-let centerY;
-let startAngle = 0;
-
-class Ring {
-    update() {
-        this.draw();
-    }
-
-    draw() {
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 8;
-        ctx.beginPath();
-        
-        for (let i = -180; i < 180; i++) {
-            let currAngleInRadians = (i + startAngle) * Math.PI / 180;
-            let displacement = 0;
-            let now = Math.abs(i);
-            
-            if (now > 70) {
-                displacement = (now - 70) / 70;
-            }
-            
-            if (displacement >= 1) {
-                displacement = 1
-            }
-            
-            let waveAmplitude = ringRadius + displacement * Math.sin(currAngleInRadians * numOfWavesPerRing) * maxWavesAmplitude;
-            let x = centerX + Math.cos(currAngleInRadians) * waveAmplitude;
-            let y = centerY + Math.sin(currAngleInRadians) * waveAmplitude;
-            
-            if (i > -180) {
-                ctx.lineTo(x, y);
-            } else {
-                ctx.moveTo(x, y);
-            }
-        }
-
-        ctx.closePath();
-        ctx.stroke();
-    }
-}
+let centerX = 0;
+let centerY = 0;
 
 function init() {
-    canvas.width = document.documentElement.clientWidth;
-    canvas.height = document.documentElement.clientHeight;
-
-    centerX = canvas.width / 2;
-    centerY = canvas.height / 2;
-
-    ringsArr = [];
-
-    for (let i = 0; i < numOfRings; i++) {
-        ringsArr.push(new Ring());
-    }
+  canvas.width  = innerWidth;
+  canvas.height = innerHeight;
+  centerX = canvas.width  / 2;
+  centerY = canvas.height / 2;
 }
 
 init();
 
-(function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+const numberOfRings     = 3;
+const ringRadiusOffset  = 7;
+const ringRadius        = 200;
+const waveOffset        = 15;
+const colors            = [`#771111`, `#bb1111`, `#ff1111`];
+let startAngle          = 0;
 
-    // Update and draw rings
-    for (let i = 0; i < ringsArr.length; i++) {
-        ringsArr[i].update();
+function updateRings() {
+  for (let i = 0; i < numberOfRings; i++) {
+    let radius = i * ringRadiusOffset + ringRadius;
+    let offsetAngle = i * waveOffset * Math.PI / 180;
+    
+    drawRing(radius, colors[i], offsetAngle);
+  }
+
+  startAngle >= 360? startAngle = 0 : startAngle++;
+}
+
+const maxWavesAmplitude = 17;
+const numberOfWaves     = 7;
+
+function drawRing(radius, color, offsetAngle) {
+  ctx.strokeStyle = color;
+  ctx.lineWidth   = 9;
+
+  ctx.beginPath();
+  
+  for (let j = -180; j < 180; j++) {
+    let currentAngle  = (j + startAngle) * Math.PI / 180;
+    let displacement  = 0;
+    let now = Math.abs(j);
+
+    if (now > 70) {
+      displacement = (now - 70) / 70;
     }
 
-    startAngle++;
-})();
+    if (displacement >= 1) {
+      displacement = 1;
+    }
 
+    let waveAmplitude = radius + displacement * Math.sin((currentAngle + offsetAngle) * numberOfWaves) * maxWavesAmplitude;
+    let x = centerX + Math.cos(currentAngle) * waveAmplitude;
+    let y = centerY + Math.sin(currentAngle) * waveAmplitude;
+
+    if (j > -180) {
+        ctx.lineTo(x, y);
+    } else {
+        ctx.moveTo(x, y);
+    }
+  }
+
+  ctx.closePath();
+  ctx.stroke();
+}
+
+(function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  updateRings();
+  requestAnimationFrame(animate);
+})();
 
 window.onresize = init;
